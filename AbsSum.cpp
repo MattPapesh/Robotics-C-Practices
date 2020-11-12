b41ef89d4808f3c244c458663388c994f4d35f37
MattPapesh <55628349+MattPapesh@users.noreply.github.com>
2020-11-11 20:05:24 -0600

    done

--- AbsSum.cpp
+++ AbsSum.cpp
@@ -2,20 +2,67 @@
 
 #include <conio.h>
 
-int input = 0;
+    int arraySize, absoluteSum;
 
-void createArray(int arraySlots)
-{
-    printf ("Enter amount of slots for the array: ");
+    int sizeOfArray() // requests an array size.
+    {
+        system ("cls");
 
+        printf ("Enter amount of slots for the array: ");
 
-}
+        std::cin >> arraySize; 
+
+        system ("cls");
+
+        return arraySize; 
+    }
 
 int main()
 {
-    input = getch();
+    int array[sizeOfArray()]; // integer type array is created
 
-    printf("%i", input);
+        for (int i = 0; i < arraySize; i++) // The slots or indexes of the array are assigned values
+                 {
+                     int assignedValue, currentSlot = i + 1;
 
+                       printf ("Provide values for each slot. Current slot: %i out of %i \n \n Assigned Value: ", currentSlot, arraySize);
+
+                      std::cin >> assignedValue;
+
+                    array[i] = assignedValue; 
+
+                   system ("cls"); 
+                 }
+
+    for (int i = 0; i < arraySize; i++) // array is displayed and the absolute sum is calculated
+    {
+        int slotValue = array[i];
+
+        if (i == 0)
+        {
+            printf ("Your array is: { ");
+        }
+
+         printf ("%i ", slotValue);
+
+        if (slotValue < 0) //if the current index or slot of the array is negative, make it positve.
+        {
+            slotValue = slotValue*-1;
+        }
+
+        absoluteSum = absoluteSum + slotValue; // all absolutes within the slots or indexes of the array are added up.
+
+       if (i + 1 == arraySize)
+        {
+            printf ("} \n");
+        }
+        else
+        {
+            printf (", ");
+        }
+    }
+
+    printf ("\n The absolute sum is: %i", absoluteSum); // the absolute sum is diplayed. 
+   
     return 0; 
 }
